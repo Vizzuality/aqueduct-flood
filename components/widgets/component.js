632eaa17478c58a9eaf863ec4f42eec77c05a151
Andrés González <andres.gonzalez@vizzuality.com>
2019-11-11 10:01:11 +0100

    Bring widget code to app

--- components/widgets/component.js
+++ components/widgets/component.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { VegaChart } from 'vizzuality-components/dist/widgets';
+import VegaChart from 'components/widgets/vega-chart';
 import isEqual from 'lodash/isEqual';
 
 // utils
