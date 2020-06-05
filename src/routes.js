b194c0df35ba30f7b2ba2c7051fef4ba637f2b72
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2020-06-05 12:13:12 +0200

    history  routing

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
 import { NOT_FOUND } from 'redux-first-router'
 import queryString from 'query-string'
+import { createHashHistory } from 'history';
 
 export const ROUTES_CONFIG = { 
   location: state => state.router,
-  querySerializer: queryString
+  querySerializer: queryString,
+  createHistory: createHashHistory
 };
 
-const directory = process.env.REACT_APP_PROD_PATH || '/';
-
 export const ROUTES = {
-  home: `${directory}:tab?`,
-  compare: `${directory}compare/:tab?`,
-  'widget-preview': `${directory}preview/:tab/widget/:id`,
-  'cba-embed': `${directory}embed/cba`,
-  'cba-embed-compare': `${directory}embed/cba/compare`,
-  'widget-embed': `${directory}embed/:tab/widget/:id`,
+  home: `/:tab?`,
+  compare: `/compare/:tab?`,
+  'widget-preview': `/preview/:tab/widget/:id`,
+  'cba-embed': `/embed/cba`,
+  'cba-embed-compare': `/embed/cba/compare`,
+  'widget-embed': `/embed/:tab/widget/:id`,
   [NOT_FOUND]: 'NotFound'
 };
 
