eeef51d2eac5bce6b3e7a27d8fd2d89eda4f58f1
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2020-10-29 10:54:39 +0100

    new version

---
+++ build/precache-manifest.ffd6456d5354f8e2b4743d298ca69926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640301b96a53ca016f6d625e6e674b2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "980fa4409755ac850974",
+    "url": "./static/css/2.38ea5221.chunk.css"
+  },
+  {
+    "revision": "a1d6b736bedbad14aa54",
+    "url": "./static/css/main.aedbcaab.chunk.css"
+  },
+  {
+    "revision": "980fa4409755ac850974",
+    "url": "./static/js/2.7fa4e6dd.chunk.js"
+  },
+  {
+    "revision": "a1d6b736bedbad14aa54",
+    "url": "./static/js/main.12e06ab3.chunk.js"
+  },
+  {
+    "revision": "b5f6d15c8f810c047c8e",
+    "url": "./static/js/runtime-main.a27f3749.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/applications/aqueduct/floods/precache-manifest.904cbf8d6e5f7f6b5cc49d0acbe38e00.js"
+  "./precache-manifest.ffd6456d5354f8e2b4743d298ca69926.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/applications/aqueduct/floods/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
