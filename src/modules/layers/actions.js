69a9641d33080078c53cd94ac94ba0871c8964da
Liela Rotschy <lrotschy@greenriver.org>
2021-05-04 13:43:16 -0600

    Move serializer into app for inspection, add notes

--- src/modules/layers/actions.js
+++ src/modules/layers/actions.js
@@ -1,8 +1,9 @@
 // src/modules/layers/actions.js floods
 
 import { createAction, createThunkAction } from 'redux-tools';
-import WRISerializer from 'wri-json-api-serializer';
+// import WRISerializer from 'wri-json-api-serializer'; // this is from the npm package
 import * as queryString from 'query-string';
+import WRISerializer from '../../constants/serializer' // I added this to src/constants so we could mess with it
 
 // utils
 import { getUniqueVocabulary } from 'utils/hazard';
@@ -40,20 +41,41 @@ export const fetchLayers = createThunkAction('LAYERS__FETCH-HAZARD-LAYERS', () =
 // /layer?name=inunriver
 // /layer
 // /layer?page[size]=100
-    // Original: return fetch(`${process.env.REACT_APP_WRI_API_URL}/v1/dataset/${FETCH_HAZARD_DATASET_ID}/layer/vocabulary/find?${queryParams}`, {})
-    return fetch(`${process.env.REACT_APP_WRI_API_URL}/v1/dataset/${FETCH_HAZARD_DATASET_ID}/layer?page[size]=600`, {})
+    // Original:
+    // return fetch(`${process.env.REACT_APP_WRI_API_URL}/v1/dataset/${FETCH_HAZARD_DATASET_ID}/layer/vocabulary/find?${queryParams}`, {})
+    // return fetch(`${process.env.REACT_APP_WRI_API_URL}/v1/dataset/${FETCH_HAZARD_DATASET_ID}/layer?page[size]=700`, {})
+    return fetch(`${process.env.REACT_APP_WRI_API_URL}/v1/dataset/${FETCH_HAZARD_DATASET_ID}/layer?name=inuncoast_historical_nosub_hist_rp&page[size]=50`)
+    // return fetch(`${process.env.REACT_APP_WRI_API_URL}/v1/dataset/${FETCH_HAZARD_DATASET_ID}/layer?name=inuncoast_historical_wtsub_2030&page[size]=50`)
       .then((response) => {
+        console.log(queryParams) // aqueductfloods=hazards_inunriver_2010.0_historical_000000000WATCH_None_None (for default settings--these are getting set with different parameters when you change the settings, responses have layers associated with them)
         if (response.ok) return response.json();
         throw response;
       })
-      // Original: .then(response => WRISerializer(response))
-      .then(response => response.data)
-      .then((data = []) => {
-        console.log(data)
-        // Original: const layerIds = ((data[0] || {}).resources || []).map(_layer => _layer.id);
-        const layerIds = data.map(_layer => _layer.id);
-        // const layerIds = ["e8b81ef9-ed40-48df-9b8a-b3f7f019328b"]
+      // The serializer is an npm package: https://www.npmjs.com/package/wri-json-api-serializer, https://github.com/resource-watch/wri-json-api-serializer
+      // it looks like the serializer might be broken? See console.log statements in original code.
+      // ====Original plus console.log statements:
+      .then(response => {
+        console.log("RESPONSE:");
+        console.log(response);
+        console.log("Serialized response");
+        console.log(WRISerializer(response)); // as expected
+        return WRISerializer(response);
+      })
+      .then(data => {
+        console.log("DATA");
+        console.log(data); // 404s using queryParams, using all layers the app loads, but it isn't correct--it's the same no matter what you select
+        const layerIds = ((data[0] || {}).resources || []).map(_layer => _layer.id);
         const promises = layerIds.map(_layerId => fetchLayer(_layerId, FETCH_HAZARD_DATASET_ID));
+      //==== end original
+
+      // +++++Our code: using this code, the flood magnitude selector in the bottom right loads--doesn't load using serializer
+      // .then(response => response.data)
+      // .then((data = []) => {
+      //   console.log(data)
+      //   const layerIds = data.map(_layer => _layer.id);
+      //   const promises = layerIds.map(_layerId => fetchLayer(_layerId, FETCH_HAZARD_DATASET_ID));
+      // +++++end our code
+      // })
 
         Promise.all(promises)
           .then((_layers => {
